ac6efc2f24a918cc01b40a99c1fcdcaf7f23935e
mugunth491 <mugunthsumathi123@gmail.com>
2023-06-13 11:56:45 +0530

    all files added

---
+++ src/pages/Home.js
@@ -0,0 +1,46 @@
+import React from "react";
+import LinkedInIcon from "@material-ui/icons/LinkedIn";
+import EmailIcon from "@material-ui/icons/Email";
+import GithubIcon from "@material-ui/icons/GitHub";
+import "../styles/Home.css";
+
+function Home() {
+  return (
+    <div className="home">
+      <div className="about">
+        <h2> Hi, My Name is Mugunth</h2>
+        <div className="prompt">
+          <p>A Data Scientist with a passion for learning and creating.</p>
+          <LinkedInIcon />
+          <EmailIcon />
+          <GithubIcon />
+        </div>
+      </div>
+      <div className="skills">
+        <h1> Skills</h1>
+        <ol className="list">
+          <li className="item">
+            <h2> Front-End</h2>
+            <span>
+              ReactJS, Angular, Redux, HTML, CSS, React Native, Flutter, NPM,
+              Ionic, BootStrap, MaterialUI, Yarn, TailwindCSS, StyledComponents
+            </span>
+          </li>
+          <li className="item">
+            <h2>Data Science</h2>
+            <span>
+              Machine Learning, Deep Learning, Data preprocessing, numpy, pandas, SQL, 
+              NLP,computer vision 
+            </span>
+          </li>
+          <li className="item">
+            <h2>Languages</h2>
+            <span>JavaScript, Java, Python, R, C, C++</span>
+          </li>
+        </ol>
+      </div>
+    </div>
+  );
+}
+
+export default Home;
